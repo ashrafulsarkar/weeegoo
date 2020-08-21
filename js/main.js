d02f5e755afbf691e06d48bbf5cbecd074b025b4
Ashraful Sarkar <58996396+ashrafulsarkar@users.noreply.github.com>
2020-08-22 03:20:27 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function ($) {
+    "use strict";
+    // meanmenu
+    $('#mobile-menu').meanmenu({
+        meanMenuContainer: '.mobile-menu',
+        meanScreenWidth: "767"
+    });
+})(jQuery);
